2efef554cdb3d3159b18572e6cb2a4e2f7e2e2e4
Mark Rivera <Mrivera1991@gmail.com>
2020-12-08 17:25:12 -0800

    added state logic, successfully getting data from backend API, implemented getIncidentCount function

--- src/components/graphs/GraphContainer.js
+++ src/components/graphs/GraphContainer.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useIncidents } from '../../state/query_hooks/useIncidents';
+
+const getIncidentCount = (data, state, setIncidentCount) => {
+  let count = 0;
+  if (!state) {
+    data.forEach(incident => {
+      count++;
+    });
+  }
+
+  return setIncidentCount(count);
+};
 
 const LineGraph = props => {
   const incidents = useIncidents();
-  const data = incidents.data;
+  const data = incidents.data && !incidents.isError ? incidents.data : [];
+  const [usState, setUsState] = useState(null);
+  const [incidentCount, setIncidentCount] = useState(0);
+
+  useEffect(() => {
+    getIncidentCount(data, usState, setIncidentCount);
+  }, [usState, data]);
 
   console.log(data);
   return <div></div>;
