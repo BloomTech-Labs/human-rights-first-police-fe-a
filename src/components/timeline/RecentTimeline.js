17ae6fccf984a2452feb6e29bcde617efa96a90f
Jennifer Stewart <jstewart8053@gmail.com>
2020-12-12 23:59:15 -0600

    added timeline and details

--- src/components/timeline/RecentTimeline.js
+++ src/components/timeline/RecentTimeline.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import { useQuery } from 'react-query';
 import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 import Moment from 'react-moment';
 import { useTimeline } from '../../state/query_hooks/useTimeline';
@@ -9,18 +6,37 @@ import { useTimeline } from '../../state/query_hooks/useTimeline';
 export function RecentTimeline() {
   const timelineQuery = useTimeline();
   return timelineQuery.isSuccess ? (
-    <div>
-      Timeline
-      <ul>
+    <div className="timeline-container">
+      <h1> Timeline of Recent Events </h1>
+      <Timeline lineColor={'#</Timeline>ddd'}>
         {timelineQuery.data.map(details => {
           return (
-            <div>
-              <div>{details.city}</div>
-              <div>{details.state}</div>
-            </div>
+            <TimelineItem
+              key={details.id}
+              dateText={<Moment format="LL">{details.date}</Moment>}
+              style={{ color: '#BC541E' }}
+              dateInnerStyle={{ color: 'white', backgroundColor: '#003767' }}
+            >
+              <h3>{details.title}</h3>
+              <h4>
+                {details.city}, {details.state}
+              </h4>
+              <div className="categories">
+                {details.categories.map(element => (
+                  <span className="category-item">{element}</span>
+                ))}
+              </div>
+              <div className="timeline-links">
+                {details.src.splice(0, 3).map(element => (
+                  <a href={element} className="link-button">
+                    Source
+                  </a>
+                ))}
+              </div>
+            </TimelineItem>
           );
         })}
-      </ul>
+      </Timeline>
     </div>
   ) : null;
 }
