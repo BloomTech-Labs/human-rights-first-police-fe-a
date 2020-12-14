4ffb24ff4b3e28ea93998169898dcc76359d44ce
Jennifer Stewart <jstewart8053@gmail.com>
2020-12-14 01:29:49 -0600

    initial commit

---
+++ src/components/timeline/Modal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+export default function ModalTimeline() {
+  return <div></div>;
+}
--- src/components/timeline/RecentTimeline.js
+++ src/components/timeline/RecentTimeline.js
@@ -22,6 +22,7 @@ export function RecentTimeline() {
           return (
             <TimelineItem
               key={nanoid()}
+              description={details.desc}
               dateText={DateTime.fromISO(details.date).toLocaleString(
                 DateTime.DATE_FULL
               )}
