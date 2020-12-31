97bd5eb0bfc4ff916e6c7e1b0d1da990bc40b53f
Jen Stewart <jstewart8053@gmail.com>
2020-12-30 21:36:32 -0600

    updated Nav to link to all other pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import '../styles/nav.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+// import logo from '../assets/humanRightsLogo.svg';
 
 import { Layout, Menu } from 'antd';
 
@@ -8,14 +11,27 @@ const { Header } = Layout;
 
 const NavBar = () => {
   return (
-    <Layout className="layout">
-      <div className="logo" />
-      <Menu mode="horizontal">
-        <Menu.Item key="1">Home</Menu.Item>
-        <Menu.Item key="2">Graphs</Menu.Item>
-        <Menu.Item key="3">About</Menu.Item>
-      </Menu>
-    </Layout>
+    <nav>
+      <Layout className="layout">
+        <Menu mode="horizontal">
+          <Menu.Item key="1">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+
+          <Menu.Item key="2">
+            <Link to="/incidents">Incidents </Link>
+          </Menu.Item>
+
+          <Menu.Item key="3">
+            <Link to="/graph">Graphs</Link>
+          </Menu.Item>
+
+          <Menu.Item key="4">
+            <Link to="/about">About </Link>
+          </Menu.Item>
+        </Menu>
+      </Layout>
+    </nav>
   );
 };
 export default NavBar;
