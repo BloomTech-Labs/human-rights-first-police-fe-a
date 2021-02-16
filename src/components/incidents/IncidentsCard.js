02d686424cdff752dd1cf8ddde8e583f02789af5
Victoria Mount <victoriamount01@gmail.com>
2021-02-16 17:00:07 -0600

    Moved date and cityState to card header, added categories and sources to card body, and added a tooltip from ANT to the IncidentsCard.

--- src/components/incidents/IncidentsCard.js
+++ src/components/incidents/IncidentsCard.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import { DateTime } from 'luxon';
-import { Card } from 'antd';
+import { Card, Popover, Button } from 'antd';
 // import '../NavBar/node_modules/antd/dist/antd.css';
 
 const IncidentsCard = props => {
   let cityState = `${props.incident.city}, ${props.incident.state}`;
+
+  function getDomain(url) {
+    url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+    url = url.split('.');
+    url = url.slice(url.length - 2).join('.');
+    if (url.indexOf('/') !== -1) {
+      return url.split('/')[0];
+    }
+    return url;
+  }
+
+  const popoverContent = (
+    <div>
+      {!props.incident.src || props.incident.src === [] ? (
+        <p>No sources listed</p>
+      ) : (
+        props.incident.src.map(source => {
+          return (
+            <div>
+              <a href={source} target="_blank" rel="noopener noreferrer">
+                {getDomain(source)}
+              </a>
+              <br />
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 
   console.log(props.incident);
 
@@ -12,16 +41,22 @@ const IncidentsCard = props => {
     <div className="card-box">
       {/* for states, I was thinking we could import the abbreviations from the bargraphAssets file */}
 
-      <Card title={cityState}>
-        <p style={{ color: '#bc541e', fontWeight: 'bold' }}>
-          {DateTime.fromISO(props.incident.date)
-            .plus({ days: 1 })
-            .toLocaleString(DateTime.DATE_MED)}
-        </p>
-
-        {/* below should be in bottom half of card */}
+      <Card
+        title={
+          <p>
+            {DateTime.fromISO(props.incident.date)
+              .plus({ days: 1 })
+              .toLocaleString(DateTime.DATE_MED)}
+          </p>
+        }
+        extra={cityState}
+      >
         <h4>{props.incident.title}</h4>
+        <p>{props.incident.categories.join(' | ')}</p>
         <p>{props.incident.desc}</p>
+        <Popover content={popoverContent} placement="rightTop">
+          <Button type="primary">Sources</Button>
+        </Popover>
       </Card>
     </div>
   );
