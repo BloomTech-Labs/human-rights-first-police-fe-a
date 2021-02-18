f1720cdc32bfcaf04a7a466bbd247cb0d71cc16b
Maria Olsen <69918976+mcolsen@users.noreply.github.com>
2021-02-18 10:40:40 -0700

    Fix crash caused by unconfigured Okta env vars

--- src/hooks/useOktaRedux.js
+++ src/hooks/useOktaRedux.js
@@ -8,26 +8,37 @@ const { setUser } = userActions;
 // user data and tokens stored in the Redux store as appropriate
 export default function useOktaRedux() {
   const dispatch = useDispatch();
-  const { authState, oktaAuth } = useOktaAuth();
+  const { authState, oktaAuth } = useOktaAuth
+    ? useOktaAuth
+    : { authState: null, oktaAuth: null };
 
   useEffect(() => {
     const updateState = async () => {
-      const { isAuthenticated, isPending, accessToken, idToken } = authState;
-      const updates = {
-        status: { authenticated: isAuthenticated, pending: isPending },
-      };
+      if (authState && oktaAuth) {
+        // Bandaid fix to prevent production from crashing due to unspecified Okta environment variables
+        // Conditional statement prevents destructuring useOktaAuth variables when undefined
+        const { isAuthenticated, isPending, accessToken, idToken } = authState;
+        const updates = {
+          status: { authenticated: isAuthenticated, pending: isPending },
+        };
 
-      if (accessToken || idToken) {
-        updates.tokens = { access: accessToken, id: idToken };
+        if (accessToken || idToken) {
+          updates.tokens = { access: accessToken, id: idToken };
+        }
+
+        if (isAuthenticated) {
+          updates.info = await oktaAuth.getUser();
+        }
+
+        dispatch(setUser(updates));
+      } else {
+        dispatch(
+          setUser({
+            status: { authenticated: false, pending: false },
+          })
+        );
       }
-
-      if (isAuthenticated) {
-        updates.info = await oktaAuth.getUser();
-      }
-
-      dispatch(setUser(updates));
     };
-
     updateState();
   }, [authState, oktaAuth, dispatch]);
 }
