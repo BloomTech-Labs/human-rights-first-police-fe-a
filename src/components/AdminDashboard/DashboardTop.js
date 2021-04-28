c12caa3e6cf83c3d59e2899b3fb9e93aa8978dfc
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2021-04-28 17:22:08 -0400

    graph no ltr

--- src/components/AdminDashboard/DashboardTop.js
+++ src/components/AdminDashboard/DashboardTop.js
@@ -4,23 +4,15 @@ const DashboardTop = props => {
   const { unapprovedIncidents, toggleAddIncident, unapproved } = props;
   return (
     <div>
-      <h2 id="admin-dashboard-title">
-        {unapproved ? 'Unapproved Incidents' : 'Approved Incidents'}
-      </h2>
-      {/* <div className="statboxes">
-        <div className="statbox">
-          <p>
-            There are currently {unapprovedIncidents.length} unapproved
-            incidents are awaiting your review. 'Select All' to approve all
-            incidents with the click of a button or alternatively, select 'More
-            Info' to inspect, edit, approve or disapprove incidents 1 by 1.
-          </p>
-          <p>
-            You can manually create an incident by using the 'Create New
-            Incident' button
-          </p>
-        </div>
-      </div> */}
+      {unapproved ? (
+        <h2 id="admin-dashboard-title">
+          {' '}
+          Unapproved Incidents: {unapprovedIncidents.length}
+        </h2>
+      ) : (
+        <h2 id="admin-dashboard-title"> 'Approved Incidents'</h2>
+      )}
+
       <div className="confirmation-message-div">
         <div className="incidents-wrap">
           <button id="create-incident-button" onClick={toggleAddIncident}>
