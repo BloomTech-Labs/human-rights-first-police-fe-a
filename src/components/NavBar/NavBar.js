f1b297e47a8c7c8f2d0059460d1d67d8a037ca21
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-05-14 05:00:27 -0400

    Fixed the Layout problem

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ import lambdaLogo from '../../assets/LambdaAssets/Built by lambda.png';
 import IncidentFocus from '../Home/Map/IncidentFocus';
 // import { useOktaAuth } from '@okta/okta-react';
 import { Layout, Menu, Sider, Input, Space, Typography } from 'antd';
-import { AudioOutlined } from '@ant-design/icons';
+import { SearchOutlined } from '@ant-design/icons';
 import './nav.css';
 const { Search } = Input;
 
@@ -21,15 +21,35 @@ const NavBar = () => {
   };
 
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        background: '#2f54eb',
+        textAlign: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <div>
+        <img
+          className="hrf-logo"
+          alt="hrf-logo"
+          src={logo}
+          style={{ width: '20rem', paddingLeft: '1rem' }}
+        />
+        <span style={{ color: 'white', paddingTop: '10rem' }}>|</span>
+        <Input
+          prefix={<SearchOutlined style={{ color: 'white' }} />}
+          size="small"
+          style={{ width: '10rem', background: 'transparent', border: 'none' }}
+          placeholder="Search incidents"
+        />
+      </div>
+
       <Menu
         mode="horizontal"
-        style={{ background: '#2f54eb', padding: '1rem' }}
+        style={{ background: '#2f54eb', paddingTop: '1rem' }}
       >
-        <Space>
-          <Title style={{ color: 'white' }}>human rights first</Title>
-          <Search placeholder="Search incidents" />
-        </Space>
+        <Space></Space>
         <Menu.Item key="1">
           <Link to="/" style={{ color: 'white' }}>
             Home
@@ -50,7 +70,6 @@ const NavBar = () => {
             About
           </Link>
         </Menu.Item>
-        <IncidentFocus />
       </Menu>
     </div>
     // <nav className="NavbarItems">
