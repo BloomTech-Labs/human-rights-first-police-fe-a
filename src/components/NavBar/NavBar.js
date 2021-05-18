c8d75000852227b1b74e289bb2dde58cb981a7e2
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-05-17 21:27:06 -0400

    Cleaned up a bit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -72,38 +72,6 @@ const NavBar = () => {
         </Menu.Item>
       </Menu>
     </div>
-    // <nav className="NavbarItems">
-    //   <img className="hrf-logo" alt="hrf-logo" src={logo}></img>
-    //   <div className="menu-icon" onClick={handleClick}>
-    //     <i className={navState ? 'fas fa-times' : 'fas fa-bars'}></i>
-    //   </div>
-    //   <ul className={navState ? 'nav-menu active' : 'nav-menu'}>
-    //     <NavLink onClick={handleClick} className="nav-link" exact to="/">
-    //       Home
-    //     </NavLink>
-    //     <NavLink onClick={handleClick} className="nav-link" to="/incidents">
-    //       Incidents
-    //     </NavLink>
-    //     <NavLink onClick={handleClick} className="nav-link" to="/graph">
-    //       Graphs
-    //     </NavLink>
-    //     <NavLink onClick={handleClick} className="nav-link" to="/about">
-    //       About
-    //     </NavLink>
-    //     {localStorage.getItem('okta-token-storage') ? (
-    //       <NavLink
-    //         onClick={handleClick}
-    //         className="nav-link"
-    //         to="/admin-dashboard"
-    //       >
-    //         Admin
-    //       </NavLink>
-    //     ) : (
-    //       <div></div>
-    //     )}
-    //     <img className="lambda-logo" src={lambdaLogo} alt="lambda-logo" />
-    //   </ul>
-    // </nav>
   );
 };
 export default NavBar;
