5dd90329461de5140e955b8e412a7d8e2b028ebb
Robert Petersen <robert.petersen808@gmail.com>
2021-05-19 18:10:56 -0500

    Created the new classnames and styles for the revamped about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './About.css';
+import confirmIcon from '../../assets/the-confirm-icon.png';
+import gatherIcon from '../../assets/gather-icon.png';
+import categorizeIcon from '../../assets/categorize-icon.png';
 
 import StudentCredits from './StudentCredits';
 
@@ -84,21 +87,25 @@ const About = () => {
     <div className="about-page-container">
       <div className="about-top-container">
         <div className="data-description-container">
-          <h2>How our data is crowdsourced</h2>
-          <p>
-            The data for this website originates from multipe sources and is
-            categorized by ML techs and approved by a member of the Blue Witness
-            Administrative Team.
+          <h1>How Our Data Is Collected</h1>
+          <p className="h1-text">
+            The data for this website is crowdsourced from multiple sources and
+            is categorized by ML techs and approved by a member of the Blue
+            Witness Administrative Team.
           </p>
         </div>
 
-        <div className="">
-          <div className="gather-container">
-            <h3>Gather</h3>
-            <ul>
+        <div className="data-info-container">
+          <div className="info-container">
+            <img className="icons" src={gatherIcon} alt="" />
+            <h2>Gather</h2>
+            <p>
               The data used for this website originates from multiple sources:
+            </p>
+            <ul>
               <li>
                 <a
+                  className="about-links"
                   href="https://github.com/2020PB/police-brutality"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -113,15 +120,12 @@ const About = () => {
                 intervention with a twitter bot using a technique called Natural
                 Language Processing.
               </li>
-              <li>
-                Manual - Manually inputed data by the Blue Witness
-                Administrative Team.
-              </li>
             </ul>
           </div>
 
-          <div className="categorize-container">
-            <h3>Categorize</h3>
+          <div className="info-container">
+            <img className="icons" src={categorizeIcon} alt="" />
+            <h2>Categorize</h2>
             <p>
               We have generated a model with in our data base to catergorize
               each incident report we recive. Because our system is in beta,
@@ -134,8 +138,13 @@ const About = () => {
             </p>
           </div>
 
-          <div className="confirm-container">
-            <h3>Confirm</h3>
+          <div className="info-container">
+            <img
+              className="confirm-icon"
+              src={confirmIcon}
+              alt="confirm icon"
+            />
+            <h2>Confirm</h2>
             <p>
               If a gathered incident report meets the criteria of a valid
               "police brutallity" incident, then it's marked to be reviewed.
