0677f315ac12871f65d6ae433cec6a78ef7cc892
Robert Petersen <robert.petersen808@gmail.com>
2021-05-25 13:59:50 -0500

    Removed unused commented out code, added needed comments, and removed unused imports in Footer.js and .css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import 'antd/dist/antd.css';
-import { Link, NavLink } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { NavLink } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
 import { useHistory } from 'react-router-dom';
 
@@ -18,59 +16,6 @@ const Footer = () => {
     push('/');
     window.location.reload();
   };
-
-  // return (
-  //   <div className="footer-container">
-  //     <div className="nav-footer">
-  //       <nav>
-  //         <Layout className="layout">
-  //           <div className="copyright-container">
-  //             <Menu mode="horizontal" className="override">
-  //               <Menu.Item key="1">
-  //                 <a
-  //                   href="https://www.humanrightsfirst.org"
-  //                   target="_blank"
-  //                   exact
-  //                   className="nav-link"
-  //                   activeClassName="active-nav-link"
-  //                   rel="noreferrer"
-  //                 >
-  //                   Human Rights First
-  //                 </a>
-  //               </Menu.Item>
-
-  //               <Menu.Item key="2">
-  //                 <NavLink
-  //                   to="/admin-dashboard"
-  //                   activeClassName="active-nav-link"
-  //                 >
-  //                   Administration
-  //                 </NavLink>
-  //               </Menu.Item>
-  // {authState.isAuthenticated && (
-  //   <Menu.Item key="3" className="logout" onClick={logout}>
-  //     <NavLink to="/" activeClassName="active-nav-link">
-  //       Log out
-  //     </NavLink>
-  //   </Menu.Item>
-  // )}
-  //             </Menu>
-  //             <p>Human Rights First &copy;2021</p>
-  //           </div>
-  //         </Layout>
-  //       </nav>
-  //     </div>
-  //     {/* <Layout className="layout"> */}
-  //     {/* <div className="hrf-container">
-  //         <a href="https://www.humanrightsfirst.org" target="_blank">Human Rights First</a>
-  //       </div>
-  //       <div className="admin-dash-container">
-  //         <Link to="/admin-dashboard">Administration</Link>
-  //       </div> */}
-
-  //     {/* </Layout> */}
-  //   </div>
-  // );
 
   return (
     <div className="footer-container">
@@ -119,6 +64,7 @@ const Footer = () => {
               Lambda School
             </a>
             <br />
+            {/* Do we want to include the Labs GitHub? */}
             {/* <a
               href="https://github.com/Lambda-School-Labs"
               target="_blank"
