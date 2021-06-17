7424bd97819387167b750e662c3542bc9a5b9687
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-06-17 09:57:44 -0700

    tests user can type username and password

--- src/components/Login/LoginContainer.test.js
+++ src/components/Login/LoginContainer.test.js
@@ -19,6 +19,24 @@ describe('<LoginContainer />', () => {
     expect(container.querySelector('#sign-in-widget')).toBeTruthy();
     expect(container.querySelector('#okta-sign-in')).toBeTruthy();
   });
+  test('user can type a username and password', async () => {
+    await render(<LoginContainer />);
+    const usernameInput = await screen.findByLabelText(/username/i);
+    const passwordInput = await screen.findByLabelText(/password/i);
+
+    const initialValue = '';
+    const usernameValue = 'foobar';
+    const passwordValue = '1234';
+
+    expect(usernameInput).toHaveValue(initialValue);
+    expect(passwordInput).toHaveValue(initialValue);
+
+    userEvent.type(usernameInput, usernameValue);
+    userEvent.type(passwordInput, passwordValue);
+
+    expect(usernameInput).toHaveValue(usernameValue);
+    expect(passwordInput).toHaveValue(passwordValue);
+  });
   test('Component responds with error message due to no credentials provided', async () => {
     await render(<LoginContainer />);
     const submitButton = await screen.findByText(/sign in/i);
