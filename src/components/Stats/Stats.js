75ed1d48d782808921763b1194cc20ea05f498fd
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-06-21 13:12:19 -0400

    cleans up code and adds comments co-author: @Stephanie

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,51 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Layout, Button, Typography } from 'antd';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Layout, Typography } from 'antd';
 import './Stats.css';
 
-const { Title, Paragraph } = Typography;
+const { Title } = Typography;
 const { Content } = Layout;
 
 const Stats = () => {
-  const [gasAndSpray, setGasAndSpray] = useState(0);
-  const [arrests, setArrests] = useState(0);
-  const [numCities, setNumCities] = useState(0);
-  // Banner Style settings
-  const contentStyle = {
-    padding: '3rem',
-    color: 'white',
-    textAlign: 'center',
-    background: '#003767',
-  };
-  // Documents the reports of incidents and sores them in a state in which it can be displayed on the Banner
-  const dataList = useSelector(state => Object.values(state.incident.data));
-
-  useEffect(() => {
-    let newCat = [];
-
-    const newCategories = dataList.map((gas, index) => {
-      return newCat.push(gas.categories.flat());
-    });
-    let pepperSpray = newCat.flat().filter((filters, index) => {
-      return filters === 'pepper-spray';
-    });
-    let tearGas = newCat.flat().filter((filters, index) => {
-      return filters === 'tear-gas';
-    });
-    let totalCities = dataList.map(city => {
-      return city.city;
-    });
-    let reducedCities = [...new Set(totalCities)];
-    let totalArrests = newCat.flat().filter((filters, index) => {
-      return filters === 'arrest';
-    });
-
-    setGasAndSpray(pepperSpray.length + tearGas.length);
-    setArrests(totalArrests.length);
-    setNumCities(reducedCities.length);
-  }, [dataList]);
-
   return (
     <Content className="BW-container" Dark="On" type="Primary">
       <div className="site-layout-background">
@@ -57,21 +17,6 @@ const Stats = () => {
         </a>
         <div className="line-break"></div>
         <Title id="title">Blue Witness Statistics</Title>
-        {/* <Paragraph style={{ color: 'white' }}>
-          {dataList.length} incidents of police use of force. {gasAndSpray} uses
-          of pepper-spray or tear-gas. {numCities} cities across the United
-          States.
-        </Paragraph>
-        <Button style={{ margin: '.5rem' }} shape="round" ghost>
-          <Link to="/incident-reports">Incident Reports</Link>
-        </Button>
-        <Button
-          style={{ marginLeft: '3rem', padding: '0 3rem' }}
-          shape="round"
-          ghost
-        >
-          <Link to="/about">About</Link>
-        </Button> */}
       </div>
     </Content>
   );
