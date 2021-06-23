7a7284c1e26cffff6385b372b483c20500e929a8
Stephanie <stephanie.enciso13@gmail.com>
2021-06-23 14:16:41 -0500

    fixes the ant-collapse css media queries since it was updating the styles in many pages and not just the home page. Co-authors: Hairo Garcia, Didi Garcia, and Joshua Samaniego

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Layout } from 'antd';
 import './Stats.css';
 
-
 const { Content } = Layout;
 
 const Stats = () => {
@@ -10,13 +9,15 @@ const Stats = () => {
     <Content className="BW-container" Dark="On" type="Primary">
       <div className="site-layout-background">
         <p className="more-info">Click arrows for more infomation</p>
-        <a href="#title" className="arrow-container">
+        <a href="#bw-title" className="arrow-container">
           <div className="arrow"></div>
           <div className="arrow"></div>
           <div className="arrow"></div>
         </a>
         <div className="line-break"></div>
-        <h2 id="title">Blue Witness Statistics</h2>
+        <h2 className="blue-witness-title" id="bw-title">
+          Blue Witness Statistics
+        </h2>
       </div>
     </Content>
   );
