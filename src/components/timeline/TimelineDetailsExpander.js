5863aa3ac66987e7ebe2c7142dad9253482e17fc
Kelly Jerrell <kcjerrell@gmail.com>
2021-09-08 14:57:57 -0700

    formatting fix

--- src/components/timeline/TimelineDetailsExpander.js
+++ src/components/timeline/TimelineDetailsExpander.js
@@ -2,27 +2,27 @@ import { DownOutlined, UpOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
 
 const TimelineDetailsExpander = props => {
-	const { children } = props;
-	const [isVisible, setIsVisible] = useState(false);
+  const { children } = props;
+  const [isVisible, setIsVisible] = useState(false);
 
-	return (
-		<>
-			{isVisible ? (
-				<div>
-					{children}
-					<UpOutlined
-						className="timeline-button"
-						onClick={() => setIsVisible(false)}
-					/>
-				</div>
-			) : (
-				<DownOutlined
-					className="timeline-button"
-					onClick={() => setIsVisible(true)}
-				/>
-			)}
-		</>
-	);
+  return (
+    <>
+      {isVisible ? (
+        <div>
+          {children}
+          <UpOutlined
+            className="timeline-button"
+            onClick={() => setIsVisible(false)}
+          />
+        </div>
+      ) : (
+        <DownOutlined
+          className="timeline-button"
+          onClick={() => setIsVisible(true)}
+        />
+      )}
+    </>
+  );
 };
 
 export default TimelineDetailsExpander;
