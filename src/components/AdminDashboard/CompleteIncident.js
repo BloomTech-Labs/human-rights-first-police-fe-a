c05016913323058d57451ff5d0e794b049598817
Kelly Jerrell <kcjerrell@gmail.com>
2021-09-08 14:45:11 -0700

    restructured CompleteIncident

--- src/components/AdminDashboard/CompleteIncident.js
+++ src/components/AdminDashboard/CompleteIncident.js
@@ -1,18 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'antd';
+import { nanoid } from 'nanoid';
 
 import AntModal from './AntModalComponent/AntModal';
-
-import { Button } from 'antd';
-
-import EmbedSource from '../EmbedSource';
-
-import { applyEdits, getData } from '../../utils/DashboardHelperFunctions';
-import { AntDesignOutlined } from '@ant-design/icons';
 import useOktaAxios from '../../hooks/useOktaAxios';
-import { nanoid } from 'nanoid';
+import { applyEdits, getData } from '../../utils/DashboardHelperFunctions';
 
 import './CompleteIncident.css';
 
+
+/**
+ * @typedef CompleteIncidentProps
+ * @property {any} incident - incident data
+ * @property {string} formattedDate - correctly formatted date
+ * @property {React.Dispatch<React.SetStateAction<any[]} setUnapprovedIncidents
+ */
+
+/**
+ *
+ * @param {CompleteIncidentProps} props
+ * @returns
+ */
 const CompleteIncident = props => {
   const {
     incident,
@@ -20,10 +28,12 @@ const CompleteIncident = props => {
     setMoreInfo,
     setUnapprovedIncidents,
   } = props;
+
   // setting state to toggle "editing mode"
   const [editing, setEditing] = useState(false);
-
   const [formValues, setFormValues] = useState({});
+
+  const oktaAxios = useOktaAxios();
 
   useEffect(() => {
     setFormValues({
@@ -35,6 +45,7 @@ const CompleteIncident = props => {
       setFormValues({});
     };
   }, [editing, incident, formattedDate]);
+
   // toggle "editing mode"
   const toggleEditor = evt => {
     evt.preventDefault();
@@ -50,8 +61,6 @@ const CompleteIncident = props => {
       [name]: value,
     });
   };
-
-  const oktaAxios = useOktaAxios();
 
   const handleSubmit = evt => {
     evt.preventDefault();
@@ -73,14 +82,79 @@ const CompleteIncident = props => {
     <div className="complete-incident">
       <div className="complete-incident-dropdown">
 
-        {/* Date */}
         {!editing ? (
-          <div className="dropdown-text-wrap">
-            <p className="complete-incident-dropdown-titles-bold">Date:</p>
-            <p>{formattedDate}</p>
-          </div>
-        ) : (
           <>
+            {/* Date */}
+            <div className="dropdown-text-wrap">
+              <p className="complete-incident-dropdown-titles-bold">Date:</p>
+              <p>{formattedDate}</p>
+            </div>
+
+            {/* Location */}
+            <div className="dropdown-text-wrap">
+              <p className="complete-incident-dropdown-titles-bold">Location:</p>
+              <p className="location-dropdown-wrap">
+                {incident.city}, {incident.state}
+              </p>
+            </div>
+
+            {/* Title */}
+            <div className="dropdown-text-wrap">
+              <p className="complete-incident-dropdown-titles-bold">
+                Title:
+              </p>
+              <p>{incident.title || '(none)'}</p>
+            </div>
+
+            {/* Description */}
+            <div className="dropdown-text-wrap">
+              <p className="complete-incident-dropdown-titles-bold">
+                Description:
+              </p>
+              <p>{incident.description}</p>
+            </div>
+
+            {/* Force Rank */}
+            <div className="dropdown-text-wrap">
+              <p className="complete-incident-dropdown-titles-bold">
+                Force Rank:
+              </p>
+              <p>{incident.force_rank}</p>
+            </div>
+
+            {/* Sources */}
+            <div className="dropdown-text-wrap">
+              <p className="complete-incident-dropdown-titles-bold">Source(s)</p>
+              <div>
+                {incident.src.map(source => (
+                  <div key={nanoid()}>
+                    <a href={source} rel="noreferrer" target="_blank">
+                      {source}
+                    </a>
+                    <br />
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Tags */}
+            <div className="dropdown-text-wrap">
+              <p className="complete-incident-dropdown-titles-bold">Tags</p>
+              <div>
+                {incident.tags ? incident.tags.join(", ") : ''}
+              </div>
+            </div>
+
+            {/* Edit button */}
+            <Button id="dropdown-edit-button" className="approve-reject-select" onClick={toggleEditor}>
+              Edit
+            </Button>
+          </>
+
+        ) : (
+
+          <>
+            {/* Date */}
             <label className="label">
               Date
               <input
@@ -91,19 +165,8 @@ const CompleteIncident = props => {
                 value={formValues.incident_date}
               />
             </label>
-          </>
-        )}
 
-        {/* Location */}
-        {!editing ? (
-          <div className="dropdown-text-wrap">
-            <p className="complete-incident-dropdown-titles-bold">Location:</p>
-            <p className="location-dropdown-wrap">
-              {incident.city}, {incident.state}
-            </p>
-          </div>
-        ) : (
-          <>
+            {/* City */}
             <label className="label">
               City
               <input
@@ -111,10 +174,11 @@ const CompleteIncident = props => {
                 onChange={handleInputChange}
                 type="text"
                 name="city"
-                value={formValues.city}
+                value={formValues.city || ''}
               />
             </label>
 
+            {/* State */}
             <label className="label">
               State
               <input
@@ -122,22 +186,11 @@ const CompleteIncident = props => {
                 onChange={handleInputChange}
                 type="text"
                 name="state"
-                value={formValues.state}
+                value={formValues.state || ''}
               />
             </label>
-          </>
-        )}
 
-        {/* Title */}
-        {!editing ? (
-          <div className="dropdown-text-wrap">
-            <p className="complete-incident-dropdown-titles-bold">
-              Title:
-            </p>
-            <p>{incident.title || '(none)'}</p>
-          </div>
-        ) : (
-          <>
+            {/* Title */}
             <label className="label">
               Title
               <input
@@ -145,22 +198,11 @@ const CompleteIncident = props => {
                 onChange={handleInputChange}
                 type="text"
                 name="title"
-                value={formValues.title}
+                value={formValues.title || ''}
               />
             </label>
-          </>
-        )}
 
-        {/* Description */}
-        {!editing ? (
-          <div className="dropdown-text-wrap">
-            <p className="complete-incident-dropdown-titles-bold">
-              Description:
-            </p>
-            <p>{incident.description}</p>
-          </div>
-        ) : (
-          <>
+            {/* Description */}
             <label className="label">
               Description
               <input
@@ -171,19 +213,8 @@ const CompleteIncident = props => {
                 value={formValues.description}
               />
             </label>
-          </>
-        )}
 
-        {/* Force Rank */}
-        {!editing ? (
-          <div className="dropdown-text-wrap">
-            <p className="complete-incident-dropdown-titles-bold">
-              Force Rank:
-            </p>
-            <p>{incident.force_rank}</p>
-          </div>
-        ) : (
-          <>
+            {/* Force Rank */}
             <label className="label">
               Force Rank
               <select
@@ -192,42 +223,16 @@ const CompleteIncident = props => {
                 name="force_rank"
                 value={formValues.force_rank}
               >
-                <option value="Rank 0 - No Police Presence">
-                  Rank 0 - No Police Presence
-                </option>
-                <option value="Rank 1 - Police Presence">
-                  Rank 1 - Police Presence
-                </option>
+                <option value="Rank 0 - No Police Presence">Rank 0 - No Police Presence</option>
+                <option value="Rank 1 - Police Presence">Rank 1 - Police Presence</option>
                 <option value="Rank 2 - Empty-hand">Rank 2 - Empty-hand</option>
                 <option value="Rank 3 - Blunt Force">Rank 3 - Blunt Force</option>
-                <option value="Rank 4 - Chemical &amp; Electric">
-                  Rank 4 - Chemical &amp; Electric
-                </option>
-                <option value="Rank 5 - Lethal Force">
-                  Rank 5 - Lethal Force
-                </option>
+                <option value="Rank 4 - Chemical &amp; Electric">Rank 4 - Chemical &amp; Electric</option>
+                <option value="Rank 5 - Lethal Force">Rank 5 - Lethal Force</option>
               </select>
             </label>
-          </>
-        )}
 
-        {/* Sources */}
-        {!editing ? (
-          <div className="dropdown-text-wrap">
-            <p className="complete-incident-dropdown-titles-bold">Source(s)</p>
-            <div>
-              {incident.src.map(source => (
-                <div key={nanoid()}>
-                  <a href={source} rel="noreferrer" target="_blank">
-                    {source}
-                  </a>
-                  <br />
-                </div>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <>
+            {/* Sources */}
             <label className="label">
               Source(s)
               <input
@@ -235,22 +240,11 @@ const CompleteIncident = props => {
                 onChange={handleInputChange}
                 type="text"
                 name="src"
-                value={formValues.src || ' '}
+                value={formValues.src || ''}
               />
             </label>
-          </>
-        )}
 
-        {/* Tags */}
-        {!editing ? (
-          <div className="dropdown-text-wrap">
-            <p className="complete-incident-dropdown-titles-bold">Tags</p>
-            <div>
-              {incident.tags ? incident.tags.join(", ") : ''}
-            </div>
-          </div>
-        ) : (
-          <>
+            {/* Tags */}
             <label className="label">
               Tags
               <span style={{ fontWeight: "normal" }}>&nbsp;(comma separated values)</span>
@@ -262,24 +256,22 @@ const CompleteIncident = props => {
                 value={formValues.tags}
               />
             </label>
+
+            {/* Cancel button */}
+            <Button id="dropdown-edit-button" className="approve-reject-select" onClick={toggleEditor}>
+              Cancel
+            </Button>
+
+            {/* Apply button */}
+            <Button className="approve-reject-select" onClick={handleSubmit}>
+              Apply Changes
+            </Button>
           </>
         )}
 
-        <Button
-          id="dropdown-edit-button"
-          className="approve-reject-select"
-          onClick={toggleEditor}
-        >
-          {editing ? 'Cancel' : 'Edit'}
-        </Button>
-        {editing && (
-          <Button className="approve-reject-select" onClick={handleSubmit}>
-            Apply Changes
-          </Button>
-        )}
         <AntModal incident={incident} />
-      </div>
-    </div>
+      </div >
+    </div >
   );
 };
 
