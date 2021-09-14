c38e3fd85944c0eb672137bfaf85d07fed6f0022
Ethan Edmond <eedmond@uccs.edu>
2021-09-14 10:31:46 -0600

    Moved graph's options to LineGraph.js instead of separate assets file

--- src/components/graphs/linegraph/LineGraph.js
+++ src/components/graphs/linegraph/LineGraph.js
@@ -1,16 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
-import { options } from '../assets';
 
 import './LineGraph.less';
 
+const initData = {
+  labels: ['Loading Data'],
+  datasets: [
+    {
+      data: [0],
+      borderColor: '#2f54eb',
+      backgroundColor: '#597ef7',
+      pointBackgroundColor: '#e63946',
+      pointBorderColor: '#95363d',
+    },
+  ],
+};
+
+const graphOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  legend: {
+    display: false,
+  },
+  scales: {
+    yAxes: [
+      {
+        stacked: false,
+        beginAtZero: true,
+        scaleLabel: {
+          fontSize: 20,
+          lineHeight: 1,
+          display: true,
+          labelString: '# of incidents',
+          padding: 0,
+        },
+      },
+    ],
+    xAxes: [
+      {
+        stacked: false,
+        scaleLabel: {
+          fontSize: 20,
+          lineHeight: 1,
+          display: true,
+          labelString: 'Months',
+          padding: {
+            bottom: 10,
+          },
+        },
+      },
+    ],
+  },
+};
+
 const LineGraph = ({ data, months }) => {
+  const [dataObj, setDataObj] = useState(initData);
   const [inCategories] = useState({});
   const [labels, setLabels] = useState([]);
-  const [label] = useState('All');
   const [lineData, setLineData] = useState([]);
 
+  console.log(months);
   useEffect(() => {
+    setDataObj({
+      ...dataObj,
+      labels: months,
+    });
     setLabels(months);
   }, [data, months]);
 
@@ -28,7 +82,7 @@ const LineGraph = ({ data, months }) => {
     }
   }, [data, labels]);
 
-  console.log(options);
+  console.log(dataObj);
   return (
     <>
       <div className="line-container">
@@ -37,7 +91,6 @@ const LineGraph = ({ data, months }) => {
             labels: labels,
             datasets: [
               {
-                label: label,
                 data: lineData,
                 borderColor: '#2f54eb',
                 backgroundColor: '#597ef7',
@@ -46,7 +99,7 @@ const LineGraph = ({ data, months }) => {
               },
             ],
           }}
-          options={options}
+          options={graphOptions}
         />
       </div>
       <p className="graph-disclaimer">
