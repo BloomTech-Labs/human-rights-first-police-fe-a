00135c044de0a265376b1ad7a9f9eb3bbf609f4f
Kelly Jerrell <kcjerrell@gmail.com>
2021-09-16 09:59:41 -0700

    remove unused imports and commented code

--- src/components/AdminDashboard/AntTableComponents/AntTable.js
+++ src/components/AdminDashboard/AntTableComponents/AntTable.js
@@ -1,26 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Space, Divider } from 'antd';
-import axios from 'axios';
-import { getData } from '../../../utils/DashboardHelperFunctions';
+import React, { useState } from 'react';
+import { Table } from 'antd';
 import CompleteIncident from '../CompleteIncident';
 import { DateTime } from 'luxon';
 
 //https://ant.design/components/table/ <---documentation on the table
 
 function AntTable(props) {
-  //const [incidents, setIncidents] = useState([]);
-  const [selectionType, setSelectionType] = useState('checkbox');
   const [approvedSelected, setApprovedSelected] = useState([]);
   const {
-    // unapprovedIncidents,
     setUnapprovedIncidents,
-    // approvedIncidents,
-    // formResponses,
     selected,
     setSelected,
-    // setCurrList,
     incidents
   } = props;
+
   function formattingDate(inputData) {
     const [year, month, day] = inputData.incident_date.split('-');
     return `${month}/${day.slice(0, 2)}/${year}`;
@@ -33,6 +26,7 @@ function AntTable(props) {
       setApprovedSelected(selectedIncidents);
     }
   };
+
   const columns = [
     //   //When DS provides data for this, uncomment for Admin Table to show %, and move.
     // {
@@ -75,23 +69,11 @@ function AntTable(props) {
     },
   ];
 
-  // provides which list of data to used based on props passed when button on the admin is pressed
-  let listToUse = incidents;
-  // if (unapprovedIncidents) {
-  //   listToUse = unapprovedIncidents;
-  //   setCurrList(unapprovedIncidents);
-  // } else if (approvedIncidents) {
-  //   listToUse = approvedIncidents;
-  // } else {
-  //   listToUse = formResponses;
-  //   setCurrList(formResponses);
-  // }
-
   return (
     <div>
       <Table
         columns={columns}
-        dataSource={listToUse}
+        dataSource={incidents}
         rowKey={'incident_id'}
         expandable={{
           expandedRowRender: incident => {
@@ -113,7 +95,7 @@ function AntTable(props) {
         }}
         pagination={{
           position: ['bottomCenter'],
-          total: listToUse ? listToUse.length : 0,
+          total: incidents ? incidents.length : 0,
           showTotal(total, range) {
             return `${range[0]}-${range[1]} of ${total} items`;
           },
--- src/components/AdminDashboard/Incidents.js
+++ src/components/AdminDashboard/Incidents.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import PendingIncident from './PendingIncident';
-import AdminTable from './AdminTable';
 import AntTable from './AntTableComponents/AntTable';
-import Incidents from '../incidents/Incidents';
 
 const DashboardIncidents = props => {
   const {
@@ -11,21 +8,15 @@ const DashboardIncidents = props => {
     setSelected,
     setUnapprovedIncidents,
     incidents,
-    // unapprovedIncidents,
-    // formResponses,
-    // setCurrList,
   } = props;
 
   return (
     <>
       <div className="incidents">
         <AntTable
-          // unapprovedIncidents={unapprovedIncidents}
           setUnapprovedIncidents={setUnapprovedIncidents}
-          // formResponses={formResponses}
           selected={selected}
           setSelected={setSelected}
-          // setCurrList={setCurrList}
           incidents={incidents}
         />
       </div>
